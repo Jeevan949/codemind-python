799349b86a1c09ef458054ead020859211575dcf
Jeevan949 <97424638+Jeevan949@users.noreply.github.com>
2022-06-29 19:10:23 +0530

    def amic(n):     s=0     for i in range(1,n):         if(n%i==0):             s=s+i     return s a=int(input()) b=int(input()) s1=amic(a) s2=amic(b) if(s1==b and s2==a):     print("Amicable") else:     print("Not Amicable")

---
+++ Amicable_Numbers.py
@@ -0,0 +1,14 @@
+def amic(n):
+    s=0
+    for i in range(1,n):
+        if(n%i==0):
+            s=s+i
+    return s        
+a=int(input())
+b=int(input())
+s1=amic(a)
+s2=amic(b)
+if(s1==b and s2==a):
+    print("Amicable")
+else:
+    print("Not Amicable")
